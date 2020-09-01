77a44e326e647f1297a4a1a0c69809c545059638
letsandeepio <sandeepchopra7@gmail.com>
2020-09-01 15:59:34 -0400

    adds Prisma and setup the db migrations

---
+++ shoppie-be/db/schema/tables.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  nominations text NULL
+);
+
